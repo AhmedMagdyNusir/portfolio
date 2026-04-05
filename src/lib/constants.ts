a2df3820dbc59fa3fb085df842b76d41c8c5473f
Ahmed Magdy <ahmedmagdynusir@gmail.com>
2026-04-05 14:48:19 +0200

    Add new technology SVG icons and update technology constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,18 +1,19 @@
 import solidIcons from "@/components/icons/solid";
 
-export const allTechnologies = {
+export const technologies = {
   JS: { name: "JavaScript", img: "/imgs/technologies/js.svg" },
   TS: { name: "TypeScript", img: "/imgs/technologies/ts.svg" },
   Vite: { name: "Vite", img: "/imgs/technologies/vite.svg" },
   React: { name: "React", img: "/imgs/technologies/react.svg" },
   Next: { name: "Next", img: "/imgs/technologies/next.svg" },
   Tailwind: { name: "Tailwind", img: "/imgs/technologies/tailwind.svg" },
-  MongoDB: { name: "MongoDB", img: "/imgs/technologies/mongodb.svg" },
+  MongoDB: { name: "MongoDB", img: "/imgs/technologies/mongo.svg" },
   Postgres: { name: "PostgreSQL", img: "/imgs/technologies/postgres.svg" },
-  NestJS: { name: "NestJS", img: "/imgs/technologies/nestjs.svg" },
-  NodeJS: { name: "Node.js", img: "/imgs/technologies/nodejs.svg" },
+  Nest: { name: "NestJS", img: "/imgs/technologies/nestjs.svg" },
+  Node: { name: "Node.js", img: "/imgs/technologies/nodejs.svg" },
   Express: { name: "Express.js", img: "/imgs/technologies/express.svg" },
   Drizzle: { name: "Drizzle ORM", img: "/imgs/technologies/drizzle.svg" },
+  ChartJS: { name: "ChartJS", img: "/imgs/technologies/chartjs.svg" },
 };
 
 export const author = {
@@ -51,7 +52,7 @@ export const projects: Project[] = [
     description:
       "A comprehensive ERP and online store solution for Leopard. The system manages inventory, orders, and customer relationships, while the online store showcases products and provides a seamless shopping experience.",
     img: "/imgs/projects/leopard-project.png",
-    technologies: [allTechnologies.Vite, allTechnologies.TS, allTechnologies.React, allTechnologies.Tailwind],
+    technologies: [technologies.TS, technologies.Postgres, technologies.Nest, technologies.Next, technologies.Tailwind],
     liveDome: "https://leopardegy.com",
   },
   {
@@ -59,7 +60,7 @@ export const projects: Project[] = [
     description:
       "A mental health platform focused on supporting Gen Z users. Provides resources, articles, and interactive tools to improve emotional, cognitive, and social wellness.",
     img: "/imgs/projects/gen-z-project.png",
-    technologies: [allTechnologies.TS, allTechnologies.React, allTechnologies.Tailwind],
+    technologies: [technologies.TS, technologies.MongoDB, technologies.Nest, technologies.Next, technologies.Tailwind],
     liveDome: "https://genzapp.com",
   },
   {
@@ -67,7 +68,7 @@ export const projects: Project[] = [
     description:
       "A CRM platform that simplifies sales, optimizes customer management, and increases productivity with lead tracking, automation, and analytics.",
     img: "/imgs/projects/pro-sales-project.jpg",
-    technologies: [allTechnologies.Vite, allTechnologies.JS, allTechnologies.React, allTechnologies.Tailwind],
+    technologies: [technologies.JS, technologies.Vite, technologies.React, technologies.Tailwind, technologies.ChartJS],
     liveDome: "https://ahmedmagdynusir.github.io/pro-sales-crm",
   },
 ];
